88f5498ac0c020f1f9c49b2cb60929ddb687fa85
Fernando Fernandez <fermen1305@gmail.com>
2022-01-11 20:36:44 -0500

    Ejemplo practico, encontrar gifs usando api publica --ANGULAR--

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, } from '@angular/core';
+import { SearchGIFResponse, Gif } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class GifsService {
+
+  private apiKey    : string = 'O6CjdeRugExYjpZMP70bmkyLyzXEFmbt';
+  private basePath  : string = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
+  get historial(){
+    return [...this._historial];
+  }
+
+  constructor( private http: HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('Historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('Resultados')!) || [];
+  }
+
+
+  buscarGifs( query: string = ''){
+   
+    query = query.trim().toLocaleLowerCase();
+
+    if(!this._historial.includes(( query ))){
+      this._historial.unshift( query );
+      this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('Historial', JSON.stringify( this._historial));
+    }
+    
+    const params = new HttpParams().set('api_key', this.apiKey).set('limit', '10').set('q',query);
+
+    this.http.get<SearchGIFResponse>(`${this.basePath}/search`,{ params })
+        .subscribe(( res ) =>{
+          this.resultados = res.data;
+          localStorage.setItem('Resultados', JSON.stringify( this.resultados));
+        });
+  }
+
+}
